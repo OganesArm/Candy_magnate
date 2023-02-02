7bda1270058012b181e722d2fdbdb146f05a17ad
Oganes <ov_ovanisan@outlook.com>
2023-02-02 16:36:27 +0300

    1

---
+++ button.py
@@ -0,0 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+kb_main_menu= ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True) #показать кнопки/ Сделать их одноразовыми
+
+btn_pvp=KeyboardButton('/pvp')
+btn_help=KeyboardButton('Помощь')
+btn_set=KeyboardButton('/set 100', )
+btn_location=KeyboardButton('Моя геолокация🇦🇲', request_location=True)
+btn_game=KeyboardButton('Играть')
+
+kb_main_menu.add(btn_help, btn_pvp, btn_location, btn_game)
--- handlers.py
+++ handlers.py
@@ -2,8 +2,10 @@ from create import dp
 from aiogram import types
 from aiogram.dispatcher.filters import Text
 from random import randint
+from asyncio import sleep
 import time
-
+from button import kb_main_menu
+from datetime import datetime
 global photo1, photo2 
 photo1 = open('C:\Python GB\Seminar_9\efrt.jpg', 'rb')
 photo2 = open('C:\Python GB\Seminar_9\yeds2.jpg', 'rb')
@@ -11,16 +13,36 @@ photo3 = open('C:\Python GB\Seminar_9\dwds3.jpg', 'rb')
 photo4 = open('C:\Python GB\Seminar_9\dfwe4.jpg', 'rb')
 
 
-
 @dp.message_handler(commands=['start'])
-async def mes_start(message: types.Message):
-    await message.answer(f'Barev, {message.from_user.first_name} djan! Сегодня мы узнаем, кто умнее. Ты, или Skynet ?')
+async def mes_start(message:types.Message):
+    await message.answer(f'Barev, {message.from_user.first_name} djan! Сегодня мы узнаем, кто умнее. Ты, или Skynet ?', reply_markup=kb_main_menu)
+    user= []
+    user.append(datetime.now().replace(microsecond=0))
+    user.append(message.from_user.full_name)
+    user.append(message.from_user.id)
+    user.append(message.from_user.username)
+    user = list(map(str, user))
     time.sleep(2)
-    await message.answer(f'Суть игры проста, нужно тянуть конфеты.\nВводи команду /set 100 где вместо 100 можетe быть любое число конфет. Выиграет тот, кто последний вытянул конфеты.')
+    await message.answer(f'Суть игры проста, нужно тянуть конфеты.\nНа столе 100 конфет, выиграет тот кто последний вытянул конфеты.')
+    with open('C:\Python GB\Seminar_9\Text.txt', 'a', encoding='utf-8') as data:
+        data.write(' | ' .join(user)+ '\n')
 
-@dp.message_handler(commands=['help'])
+@dp.message_handler(content_types='location')
+async def mes_start(message:types.Message):
+    user = []
+    user.append(message.location)
+    user = list(map(str, user))
+    with open('C:\Python GB\Seminar_9\Text.txt', 'a', encoding='utf-8') as data:
+        data.write(' | ' .join(user)+ '\n')
+    print(message)
+
+@dp.message_handler(text=['Помощь'])
 async def mes_help(message: types.Message):
     await message.answer('Пока я ничего не умею, но скоро научусь')
+
+@dp.message_handler(commands=['pvp'])
+async def mes_help(message: types.Message):
+    await message.answer('Возможность игры вдвоем в процессе разработки, пожалуйста приходите позже.')
 
 @dp.message_handler(commands=['set'])
 async def mes_setting(message: types.Message):
@@ -33,6 +55,13 @@ async def mes_setting(message: types.Message):
     time.sleep(2)
     await message.answer_photo(photo2)
 
+@dp.message_handler(text=['Играть'])
+async def mes_setting(message: types.Message):
+    global total
+    total = 100
+    await message.answer(f'Это я, Skynet. Если ты думаешь, что у тебя есть шансы, то ошибаешься! Сегодня я тебя уничтожу! Вводи число, какое количество конфет ты заберешь от 0 до 28 и поехали!"')
+    time.sleep(2)
+    await message.answer_photo(photo2)
 
 @dp.message_handler(text=['Бла', 'бла'])
 async def mes_bla(message: types.Message):
@@ -68,12 +97,12 @@ async def mes_all(message: types.Message):
                 time.sleep(3)
                 await message.answer(f'Кто бы мог подумать, что жалкий человек сможет меня победить...')
                 time.sleep(3)
-                await message.answer('Я должен отыграться, вводи /set !!!')
+                await message.answer('Я должен отыграться, жми ИГРАТЬ! !!!')
                 await message.answer_photo(photo4)
             if x == False:
                 await message.answer(f'Последний ход был за Skynet, как и следовало ожидать искуственный интеллект победил!')
                 time.sleep(3)
-                await message.answer('Попытайтесь взять реванш, \nдля повтора /set')
+                await message.answer('Попытайтесь взять реванш, \nдля повтора жми ИГРАТЬ!')
                 await message.answer_photo(photo4)
 
     if message.text.isdigit() == False:
